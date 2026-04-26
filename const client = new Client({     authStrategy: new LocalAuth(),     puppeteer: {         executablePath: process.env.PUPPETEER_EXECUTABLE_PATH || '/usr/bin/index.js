9d9dc4a42998faa8b3b73c569efb358302e1c6a4
mataniatb <53862848+mataniatb@users.noreply.github.com>
2026-04-26 16:18:48 +0300

    Delete const client = new Client({     authStrategy: new LocalAuth(),     puppeteer: {         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/index.js

--- const client = new Client({     authStrategy: new LocalAuth(),     puppeteer: {         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/index.js
+++
@@ -1,7 +0,0 @@
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    }
-});
